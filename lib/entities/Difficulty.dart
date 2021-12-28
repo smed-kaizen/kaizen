d485de5a9ee856d5b58fdfb4b3a885947102b1c5
mohammed slimani <mohammed.slimani@um5s.net.ma>
2021-12-28 15:38:17 +0100

    Adding the db interface for the difficulties entity

--- lib/entities/Difficulty.dart
+++ lib/entities/Difficulty.dart
@@ -1,11 +1,17 @@
 class Difficulty {
-  int? id;
-  final String name;
-  final int pts;
+  late final int id;
+  late final String name;
+  late final int pts;
 
   Difficulty({
-    this.id,
+    required this.id,
     required this.name,
     required this.pts
   });
+
+  Difficulty.fromMap(Map<String, Object?> map) {
+    id = map['id']! as int;
+    name = map['name'].toString();
+    pts = map['pts'] as int;
+  }
 }
